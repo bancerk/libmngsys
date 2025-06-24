77c3512e236914d075551672f6104fba75fc0487
bancerk <192424850+bancerk@users.noreply.github.com>
2025-06-24 18:04:59 +0300

    init commit

---
+++ src/test/java/dev/patika/libmngsys/LibmngsysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.libmngsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmngsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
